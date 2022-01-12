0f3f436eedd423e9a380e62fa90f7f9ea3c9dcca
Alonso Rodriguez <alonso.rodriguez.ponce@gmail.com>
2022-01-12 11:39:53 -0600

    Tipos de Datos y Variables

---
+++ tiposDeDatosYVariables/script.js
@@ -0,0 +1,69 @@
+// Part 1: Type Converter
+
+/*var valor = false;
+compare(valor);
+
+var valor = true;
+compare(valor);
+
+var valor = 0;
+compare(valor);
+
+var valor = 1;
+compare(valor);
+
+var valor = "0";
+compare(valor);
+
+var valor = "000";
+compare(valor);
+
+var valor = "1";
+compare(valor);
+
+var valor = NaN;
+compare(valor);
+
+var valor = Infinity;
+compare(valor);
+
+var valor = -Infinity;
+compare(valor);
+
+var valor = "";
+compare(valor);
+
+var valor = "20";
+compare(valor);
+
+var valor = "Twenty";
+compare(valor);
+
+var valor = null;
+compare(valor);
+
+var valor = undefined;
+compare(valor);
+
+
+
+
+function compare(valor){
+    console.log(Number(valor));
+
+    console.log(String(valor));
+
+    console.log(Boolean(valor));
+}
+
+
+
+*/
+// Challenge 2: Create an information card
+var person = {
+    name: "Juan",
+    location: "Mexico",
+    hobby: "Music"
+};
+
+console.log("Hi my name is " + person.name + ". I live in " + person.location + " and enjoy " + person.hobby);
